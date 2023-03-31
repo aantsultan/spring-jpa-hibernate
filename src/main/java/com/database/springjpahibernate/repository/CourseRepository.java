9e0ceabc539145a7c036decbc2e95d02fb7dff7d
Aant <aant.sultan@gmail.com>
2023-03-31 17:28:13 +0700

    adding function save in repository

--- src/main/java/com/database/springjpahibernate/repository/CourseRepository.java
+++ src/main/java/com/database/springjpahibernate/repository/CourseRepository.java
@@ -17,6 +17,15 @@ public class CourseRepository {
         return em.find(Course.class, id);
     }
 
+    public Course save(Course course){
+        if(course.getId() == null){
+            em.persist(course);
+        } else {
+            em.merge(course);
+        }
+        return course;
+    }
+
     public void deleteById(Long id){
         Course course = findById(id);
         em.remove(course);
